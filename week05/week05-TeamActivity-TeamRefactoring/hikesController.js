98afaec82b8066084ba272a3355940b7f9ee92b9
leonilsonlopes <leonilson.lopes1@gmail.com>
2021-02-02 23:54:18 +0000

    20210202_2354

--- week05/week05-TeamActivity-TeamRefactoring/hikesController.js
+++ week05/week05-TeamActivity-TeamRefactoring/hikesController.js
@@ -1,6 +1,9 @@
 import HikeModel from './HikeModel.js';
 import HikesView from './HikesView.js';
 
+
+alert("HikesController.js is called");
+
 // Hike controller
 export default class HikesController {
   constructor(parentId) {
--- week05/week05-TeamActivity-TeamRefactoring/index.js
+++ week05/week05-TeamActivity-TeamRefactoring/index.js
@@ -1,5 +1,7 @@
 import HikesController from './HikesController.js';
 
+alert("Index.js is called");
+
 window.addEventListener("load", () => {
   HikesController.showHikeList();
 });
