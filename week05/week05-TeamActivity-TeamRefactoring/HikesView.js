d75fd7fb80396e5464dee8b08bc54850543f803f
leonilsonlopes <leonilson.lopes1@gmail.com>
2021-02-03 00:16:44 +0000

    20210203_0016

---
+++ week05/week05-TeamActivity-TeamRefactoring/HikesView.js
@@ -0,0 +1,37 @@
+  // Hike View handler
+export default class HikesView {
+    constructor(listElementId) {
+        // will need this
+        this.imgBasePath = '//byui-cit.github.io/cit261/examples/';
+        
+    }
+  renderHikeList(hikeList, listElement) {
+    hikes.forEach(hike => {
+    listElement.appendChild(renderOneHike(hike));
+  });
+  }
+  
+  renderOneHikeLight(hike) {
+    const item = document.createElement("li");
+
+	item.innerHTML = ` <h2>${hike.name}</h2>
+			<div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+			<div>
+					<div>
+						<h3>Distance</h3>
+						<p>${hike.distance}</p>
+					</div>
+					<div>
+						<h3>Difficulty</h3>
+						<p>${hike.difficulty}</p>
+					</div>
+			</div>`;
+	
+	return item;
+  }
+  renderOneHikeFull(hike, parentElement) {
+    // this method will be used to one hike with full detail...you will need this for the stretch goal! 
+  }
+}
+
+
