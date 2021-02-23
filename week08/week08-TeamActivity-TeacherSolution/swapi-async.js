a55a5964c9110ae73eced2322386593796bd6922
leonilsonlopes <leonilson.lopes1@gmail.com>
2021-02-23 22:25:08 +0000

    20210223_2225

--- week08/week08-TeamActivity-TeacherSolution/swapi-async.js
+++ week08/week08-TeamActivity-TeacherSolution/swapi-async.js
@@ -59,7 +59,7 @@ async function showShips(url = "https://swapi.dev/api/starships/") {
   if (results.next) {
     const next = document.getElementById("next");
     // normally we would prefer the addEventListener method of adding a listener. Using something like 'element.onEvent = event_function' has the limitation of only being able to hold one listener of the type. In this case that is a good thing however. Because we are not re-creating the buttons each time we load a new batch of data we could end up with several listeners attached to each button by the last page. We won't have that issue here.
-    next.ontouchend = () => {
+    next.click = () => {
       // notice to show the next page we just re-call the showShips function with a new URL
       showShips(data.next);
     };
@@ -67,7 +67,7 @@ async function showShips(url = "https://swapi.dev/api/starships/") {
   if (results.previous) {
     const prev = document.getElementById("prev");
     // we need to set the once option on the listener since the buttons do not get recreated with each page load. If we don't we will end up with several listeners all trying to fire at once after a few pages
-    prev.ontouchend = () => {
+    prev.click = () => {
       showShips(data.previous);
     };
   }
