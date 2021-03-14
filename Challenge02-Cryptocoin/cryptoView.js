2eb6953456bbbe778e88a23cc55b89a82caa6272
leonilsonlopes <leonilson.lopes1@gmail.com>
2021-03-14 04:11:21 +0000

    x

--- Challenge02-Cryptocoin/cryptoView.js
+++ Challenge02-Cryptocoin/cryptoView.js
@@ -10,12 +10,13 @@ export default class cryptoView {
 	}
   
 	renderOneCoin(unitaryCoin) {
-		console.log("unitary coin: " + unitaryCoin);		
+		console.log("unitary coin: " + JSON.stringify(unitaryCoin));		
 		const item = document.createElement("li");
 		
 		//Set task id into this list element	
 		item.setAttribute("data-id", unitaryCoin.id);
 		
+		console.log("coin attributes: " + unitaryCoin.symbol + ": " + unitaryCoin.price);
 		//Render task content
 		item.innerHTML = unitaryCoin.symbol + ": " + unitaryCoin.price;
 		
