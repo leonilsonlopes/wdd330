fe9be8f5ebcf527ee554426e84cab8976429578f
leonilsonlopes <leonilson.lopes1@gmail.com>
2021-03-14 02:15:24 +0000

    x

--- Challenge02-Cryptocoin/index.js
+++ Challenge02-Cryptocoin/index.js
@@ -1,7 +1,7 @@
 import orchestratorController from './orchestratorController.js';
 
 window.addEventListener("load", () => {
-	const myOrchControl = new orchestratorController(); 	
+	const myOrchControl = new orchestratorController("coinList"); 	
 
 	myOrchControl.startPage();
 });
