ba307aa101f2f981d1113fa36b13bed9076941c3
leonilsonlopes <leonilson.lopes1@gmail.com>
2021-03-14 03:48:47 +0000

    x

--- week10/week10-TeamActivity-Quakes/utilities.js
+++ week10/week10-TeamActivity-Quakes/utilities.js
@@ -1,9 +1,10 @@
 export function getJSON(url) {
-  return fetch(url)
+  return fetch("https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT")
       .then(function(response) {
           if (!response.ok) {
               throw Error(response.statusText);
           } else {
+			  console.log("response: " + JSON.stringify(response));
               return response.json();
           }
       })
