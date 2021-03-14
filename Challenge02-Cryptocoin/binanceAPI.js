537404ba4cd84d7320e8ca5a9fb0821c8f045423
leonilsonlopes <leonilson.lopes1@gmail.com>
2021-03-14 03:36:24 +0000

    x

--- Challenge02-Cryptocoin/binanceAPI.js
+++ Challenge02-Cryptocoin/binanceAPI.js
@@ -10,23 +10,24 @@ export default class binanceAPI {
 	callBinanceAPI(apiGet, params) {
 		let url = apiGet + params;
 		console.log("API Call: " + url);
-  		try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw Error(response.statusText);
-            } else {
-                console.log("response: " + JSON.stringify(response));
-                return response.json();
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  		return fetch(url)
+    	  .then(function(response) {
+       	   if (!response.ok) {
+       	       throw Error(response.statusText);
+       	   } else {
+				console.log("response: " + JSON.stringify(response));
+        	    return response.json();
+         	 }
+     	 })
+    	  .catch(function(error) {
+        	  console.log(error);
+     	 });
 	}
 
 
 	async getPrice(cryptoSymbol){
-		const params = "?symbol=" + cryptoSymbol + "USDT";
-		const resultSet = await this.callBinanceAPI(this.priceQuery,params);
+		let params = "?symbol=" + cryptoSymbol + "USDT";
+		let resultSet = await this.callBinanceAPI(this.priceQuery,params);
 		return  resultSet;
 		
 	}
